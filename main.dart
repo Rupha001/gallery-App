3fd975fb840dfd243e77cb99d1193ff16ba9294f
Rupha001 <153528545+Rupha001@users.noreply.github.com>
2024-12-07 16:49:02 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:photo_gallery/photo_gal.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
+        useMaterial3: true,
+      ),
+      home: MyWidget(),
+    );
+  }
+}
